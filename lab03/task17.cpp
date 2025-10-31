6fd305d3d975abf9943e6ee0c3382753351928c1
coroutin <auhnev29@gmail.com>
2025-10-31 17:00:27 +0300

    sdat' sizifchika

--- lab03/task17.cpp
+++ lab03/task17.cpp
@@ -2,19 +2,13 @@
 
 int main() {
     long long n, sum = 0;
-    std::cin >> n;
+    std::cin >> n;//Получаем число от пользователя
     for (int i = 1; i < n; i ++) {
-        if (n % i == 0) {
-            sum += i;
-        }
+        if (n % i == 0) {sum += i;}//Проверяем что i – делитель числа n
     }
-    if (sum == n) {
-        std::cout << sum<< std::endl;
-        std::cout << "YES";
-    }
-    else if (n == 1) {std::cout << 1 << std::endl;
-        std::cout << "NO";}
-    else {std::cout << sum<< std::endl; std::cout << "NO";}
+    if (sum == n) {std::cout << sum << std::endl; std::cout << "YES";}//Проверяем, что сумма делителей числа равна числу
+    else if (n == 1) {std::cout << 1 << std::endl; std::cout << "NO";}//Проверка особого случяя, когла число равно единице
+    else {std::cout << sum << std::endl; std::cout << "NO";}//Иначе вывводим сумму делителей числа, и то что оно является несоверщенным
 
     return 0;
 }
